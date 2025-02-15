882883cf7ccc050638e3ae84929bd5574d3af69d
vlad3005 <vladborunov396@gmail.com>
2025-02-15 16:33:38 +0300

    bot version 1

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from keyboards.all_keyboards import main_kb, create_spec_kb,create_rat
+from keyboards.all_keyboards import main_kb
 
 start_router = Router()
 
@@ -11,10 +11,9 @@ async def cmd_start(message: Message):
                          reply_markup=main_kb(message.from_user.id))
 @start_router.message(Command('start_2'))
 async def cmd_start(message: Message):
-    await message.answer('Запуск сообщения по команде /start_2 используя фильтр Command()',
-                         reply_markup=create_spec_kb())
-
-@start_router.message(F.text == '/start_3')
-async def cmd_start(message: Message):
-    await message.answer('Запуск сообщения по команде /start_3 используя магический фильтр F.text!',
-                         reply_markup=create_rat())
+    await message.answer('Запуск сообщения по команде /start_2 используя фильтр Command()')
+#
+# @start_router.message(F.text == '/start_3')
+# async def cmd_start(message: Message):
+#     await message.answer('Запуск сообщения по команде /start_3 используя магический фильтр F.text!',
+#                          reply_markup=create_rat())
